cf6e9bd18e2af1343305911d8d3556a56918591c
Alisson Oliveira <alisson@coopervap.com.br>
2025-06-29 10:07:36 -0300

    ajustes para o home

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, HardSkill, SoftSkill, Language, PortfolioItem, ProfessionalExperience
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(HardSkill)
+admin.site.register(SoftSkill)
+admin.site.register(Language)
+admin.site.register(PortfolioItem)
+admin.site.register(ProfessionalExperience)
